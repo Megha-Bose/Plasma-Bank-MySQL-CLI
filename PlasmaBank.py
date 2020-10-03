3a6054b93961e2348749feb562ebd540b066d208
Megha-Bose <megh.bose@gmail.com>
2020-10-03 15:28:50 +0530

    [Add] PlasmaBank.py

---
+++ PlasmaBank.py
@@ -0,0 +1,110 @@
+import subprocess as sp
+import pymysql
+import pymysql.cursors
+
+
+def option2():
+    """
+    Function to implement option 1
+    """
+    print("Not implemented")
+
+
+def option3():
+    """
+    Function to implement option 2
+    """
+    print("Not implemented")
+
+
+def option4():
+    """
+    Function to implement option 3
+    """
+    print("Not implemented")
+
+
+def loginUser(): # to determine access
+    try:
+        # Takes login details as input
+        row = {}
+        row["Login_id"] = input("Login ID: ")
+        row["Password"] = input("Password: ")
+        if(row["Login_id"] != "Admin"):
+        	query = "SELECT * FROM USER WHERE Login_id like %s AND Password like%s" % (row["Login_id"], row["Password"])
+        	cur.execute(query)
+        con.commit()
+
+        print("Login Successful")
+
+    except Exception as e:
+        con.rollback()
+        print("Failed attempt to login")
+        print(">>>>>>>>>>>>>", e)
+
+    return
+
+
+def dispatch(ch):
+    """
+    Function that maps helper functions to option entered
+    """
+
+    if(ch == 1):
+        hireAnEmployee()
+    elif(ch == 2):
+        option2()
+    elif(ch == 3):
+        option3()
+    elif(ch == 4):
+        option4()
+    else:
+        print("Error: Invalid Option")
+
+
+# Global
+while(1):
+    tmp = sp.call('clear', shell=True)
+    
+    # Can be skipped if you want to hard core username and password
+    username = input("Username: ")
+    password = input("Password: ")
+
+    try:
+        # Set db name accordingly which have been create by you
+        # Set host to the server's address if you don't want to use local SQL server 
+        con = pymysql.connect(host='localhost',
+                              user=username,
+                              password=password,
+                              db='COMPANY',
+                              cursorclass=pymysql.cursors.DictCursor)
+        tmp = sp.call('clear', shell=True)
+
+        if(con.open):
+            print("Connected")
+        else:
+            print("Failed to connect")
+
+        tmp = input("Enter any key to CONTINUE>")
+
+        with con.cursor() as cur:
+            while(1):
+                tmp = sp.call('clear', shell=True)
+                # Here taking example of Employee Mini-world
+                print("1. Option 1")  # Hire an Employee
+                print("2. Option 2")  # Fire an Employee
+                print("3. Option 3")  # Promote Employee
+                print("4. Option 4")  # Employee Statistics
+                print("5. Logout")
+                ch = int(input("Enter choice> "))
+                tmp = sp.call('clear', shell=True)
+                if ch == 5:
+                    break
+                else:
+                    dispatch(ch)
+                    tmp = input("Enter any key to CONTINUE>")
+
+    except:
+        tmp = sp.call('clear', shell=True)
+        print("Connection Refused: Either username or password is incorrect or user doesn't have access to database")
+        tmp = input("Enter any key to CONTINUE>")
