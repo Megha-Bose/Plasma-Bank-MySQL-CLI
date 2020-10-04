f718ca1071c7d8fd9d994660748c37add742b5a2
ananya-9 <59643853+ananya-9@users.noreply.github.com>
2020-10-04 21:51:02 +0530

    one analysis fn left

---
+++ analysis.py
@@ -0,0 +1,33 @@
+# Get the donor details for a particular order (relationship between order and donor)
+# Get all successful orders that are completed by a particular vehicle type (relationship between logistics and order)
+# Get donors in a particular age group whose plasmas have been successfully delivered to a patient (this shows relationship between donor, plasma, order, hospital and patient)
+
+def getDonorOrder(cur, con):
+	try:
+		oid = input("Enter order_id: ")
+		query = "SELECT DONOR.* FROM DONOR INNER JOIN ORDER_REQUEST ON DONOR.Donor_id = ORDER_REQUEST.Donor_id WHERE ORDER_REQUEST.Order_id LIKE '%s'" %(oid)
+		if cur.execute(query):
+			print(cur.fetchall())
+		con.commit()
+
+	except Exception as e:
+		con.rollback()
+		print("Failed to find donors with max donations")
+		print(">>>>>>>>>>>>>", e)
+
+	return
+
+def getOrderVehicle(cur, con):
+	try:
+		vtype = input("Enter vehicle type: ")
+		query = "SELECT ORDER_REQUEST.* FROM ORDER_REQUEST INNER JOIN LOGISTICS ON ORDER_REQUEST.Vehicle_id = LOGISTICS.Vehicle_id WHERE ORDER_REQUEST.Accepted = 1 AND LOGISTICS.Vehicle_type LIKE '%s'" %(vtype)
+		if cur.execute(query):
+			print(cur.fetchall())
+		con.commit()
+
+	except Exception as e:
+		con.rollback()
+		print("Failed to find donors with max donations")
+		print(">>>>>>>>>>>>>", e)
+
+	return
