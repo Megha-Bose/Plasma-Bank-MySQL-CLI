e23ab2c51f4abb16967dceb7ea71a9ef2b634301
adata111 <ahana.datta.ravenclaw@gmail.com>
2020-10-08 00:02:23 +0530

    updated

--- projection.py
+++ projection.py
@@ -1,12 +1,15 @@
 # Get the names of all donors with greater than x donations
 # Get the vehicle type from logistics depending on the distance in an order
 
+import pretty
+from pretty import *
+
 def donorGrtrX(cur, con):
 	try:
 		x = int(input("Enter x: "))
 		query = "SELECT * FROM DONOR WHERE Number_of_donations > '%d' " %(x)
 		if cur.execute(query):
-			print(cur.fetchall())
+			pretty(cur.fetchall())
 		con.commit()
 
 	except Exception as e:
@@ -22,7 +25,7 @@ def getVehicle(cur, con):
 		distmax = int(input("Enter upper limit distance: "))
 		query = "SELECT Vehicle_type FROM LOGISTICS WHERE distance >= '%d' AND distance <= '%d'" %(distmin, distmax)
 		if cur.execute(query):
-			print(cur.fetchall())
+			pretty(cur.fetchall())
 		con.commit()
 
 	except Exception as e:
