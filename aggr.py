931c6c9b86f0afa07c22c30b698ae648d3fce50c
Megha-Bose <megh.bose@gmail.com>
2020-10-08 11:06:48 +0530

    Aggr fix

--- aggr.py
+++ aggr.py
@@ -38,7 +38,7 @@ def avgSal(cur,con):
 def avgDons(cur,con):
 	try:
 		year = int(input("Enter year for which you want to see average number of donations per month: "))
-		query = "SELECT AVG(num) WHERE num IN (SELECT COUNT(*),MONTH(Donation_date) AS num FROM PLASMA WHERE YEAR(Donation_date) = '%d' GROUP BY MONTH(Donation_date))"(year)
+		query = "SELECT AVG(num) FROM (SELECT COUNT(*),MONTH(Donation_date) AS num FROM PLASMA WHERE YEAR(Donation_date) = '%d' GROUP BY MONTH(Donation_date)) AS TAB;" % (year)
 		if cur.execute(query):
 			pretty(cur.fetchall())
 		con.commit()
@@ -53,7 +53,7 @@ def avgDons(cur,con):
 def avgOrders(cur,con):
 	try:
 		year = int(input("Enter year for which you want to see average number of orders placed per month: "))
-		query = "SELECT AVG(num) WHERE num IN (SELECT COUNT(Order_id),MONTH(Order_date) AS num FROM ORDER_REQUEST WHERE YEAR(Order_date) = '%d' GROUP BY MONTH(Order_date))"(year)
+		query = "SELECT AVG(num) WHERE FROM (SELECT COUNT(Order_id),MONTH(Order_date) AS num FROM ORDER_REQUEST WHERE YEAR(Order_date) = '%d' GROUP BY MONTH(Order_date)) AS TAB" %(year)
 		if cur.execute(query):
 			pretty(cur.fetchall())
 		con.commit()
